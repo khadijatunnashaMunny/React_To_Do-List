ba3fca4d6e66cb60642f37b7277568b6155ca496
khadijatunnasha munny <khadijatunnashamunny@gmail.com>
2022-02-26 08:23:50 +0600

    items added in local storage

--- reacttodolist/src/Components/ToDo.js
+++ reacttodolist/src/Components/ToDo.js
@@ -1,46 +1,69 @@
-import React, { useState } from 'react'
-import Lists from './Lists';
+import React, { useState, useEffect } from "react";
+import Lists from "./Lists";
 
 function ToDo() {
-    const[inputList,setInputList]=useState("apple");
-    const[items,setItems]=useState([]);
+  const LOCAL_STORAGE_KEY = "contacts";
 
-    const inputHandling=(e)=>{
-        setInputList(e.target.value);
-    }
-    const add=()=>{
-        setItems((oldItems)=>{
-            return [...oldItems,inputList];
-        })
-        setInputList("");
-    }
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
 
-    const deleteHandler=(id)=>{
-        console.log(id);
-        setItems((oldItems)=>{
-            return oldItems.filter((arrElem,index)=>{
-                return index !== id;
-            });
-        })
+  const inputHandling = (e) => {
+    setInputList(e.target.value);
+  };
+  const add = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
 
-    }
+  const deleteHandler = (id) => {
+    console.log(id);
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
+  useEffect(() => {
+    const retriveLists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveLists) setItems(retriveLists);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(items));
+  }, [items]);
+
   return (
-    <div className='ToDoStyle' style={{alignItems:'center',textAlign:'center',marginTop:30}}>
-        <h3>ToDo list</h3>
-        <div>
-            <input type="text" placeholder='enter name' value={inputList} onChange={inputHandling}/>
-            <button onClick={add}>Add</button>
-        </div>
+    <div
+      className="ToDoStyle"
+      style={{ alignItems: "center", textAlign: "center", marginTop: 30 }}
+    >
+      <h3>ToDo list</h3>
       <div>
-      {items.map((value,index)=>{
-            return <Lists key={index} id={index} text={value} onSelect={deleteHandler}/>        
-        })
-      }
-         
+        <input
+          type="text"
+          placeholder="enter name"
+          value={inputList}
+          onChange={inputHandling}
+        />
+        <button onClick={add}>Add</button>
       </div>
-
-        </div>
-  )
+      <div>
+        {items.map((value, index) => {
+          return (
+            <Lists
+              key={index}
+              id={index}
+              text={value}
+              onSelect={deleteHandler}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default ToDo
+export default ToDo;
